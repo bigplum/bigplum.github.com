c6cbc70d5fbb8760eb338c48f14cbe91f8885bee
lixiaoming <bigplum@gmail.com>
2012-03-21 09:35:23 +0800

    fix py

--- tools/rep.sh
+++ tools/rep.sh
@@ -1,8 +1,10 @@
-for f in `ls *markdown`; do
-    sed -e "s/\(\[bash\]\)/{% highlight bash%}/" $f |sed -e "s/\(\[\/bash\]\)/{% endhighlight %}/" | sed -e "s/\(\[c\]\)/{% highlight c %}/" | sed -e "s/\(\[\/c\]\)/{% endhighlight %}/" >> $f.new 
-    mv $f.new $f
-
-    sed -e "s/\(\&quot\;\)/"\""/g" $f | sed -e "s/\(\&amp\;\)/\&/g" | sed -e "s/\(\&gt\;\)/>/g"| sed -e "s/\(\&lt\;\)/</g" >> $f.new
-
-    mv $f.new $f
+for d in `ls _posts`; do
+    for f in `ls _posts/$d/*markdown`; do
+        sed -e "s/\(\[bash\]\)/{% highlight bash%}/" $f |sed -e "s/\(\[\/bash\]\)/{% endhighlight %}/" | sed -e "s/\(\[c\]\)/{% highlight c %}/" | sed -e "s/\(\[\/c\]\)/{% endhighlight %}/" | sed -e "s/\(\[python\]\)/{% highlight python %}/" | sed -e "s/\(\[\/python\]\)/{% endhighlight %}/" >> $f.new 
+        mv $f.new $f
+    
+        sed -e "s/\(\&quot\;\)/"\""/g" $f | sed -e "s/\(\&amp\;\)/\&/g" | sed -e "s/\(\&gt\;\)/>/g"| sed -e "s/\(\&lt\;\)/</g" >> $f.new
+    
+        mv $f.new $f
+    done
 done
